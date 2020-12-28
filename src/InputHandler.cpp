c6f729dade6cbdb3847b7896b3b6ed6b142df548
Göktuğ Ersöz <383247@cs.ktu.edu.tr>
2020-12-28 14:46:32 +0300

    InputHandler class'ının header file'ında prototipleri belirlenen fonksiyonlar source file'da düzenlendi.

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <SDL.h>
 #include "InputHandler.h"
 #include "Sim.h"
-#include <SDL.h>
-/*
-Constructorda Mouse position ve Mouse Button State'leri setlenecek.
-Diger mouse ve klavye durumlari donguye eklenecek.
-header dosyasindaki onMouseMove, onMouseButtonUp, onMouseButtonDown,
-getMousePosition, getMouseButtonState yazilacak.
-void clean() fonksiyonunu bu dosyaya da ekle. ici bos kalsin.
-*/
+
 InputHandler* InputHandler::s_pInstance = 0;
-InputHandler::InputHandler()
+InputHandler::InputHandler() : m_mousePosition(0.0,0.0) //Default olarak mouse konumu setlenir.
 {
+    for(int i = 0; i < 3; i++)
+    {
+        m_mouseButtonStates.push_back(false);
+    }
 
 }
-//inputların durumunu günceller
+//Mouse'un hangi butonda oldugunu dondurur.
+bool InputHandler::getMouseButtonState(int buttonNumber)
+{
+        return m_mouseButtonStates[buttonNumber];
+}
+//Mouse'un konumunu dondurur.
+Vector2D* InputHandler::getMousePosition()
+{
+        return &m_mousePosition;
+}
+//Inputların durumunu gunceller
 void InputHandler::update()
 {
     SDL_Event event;
@@ -26,9 +34,17 @@ void InputHandler::update()
             case SDL_QUIT:
                 TheSim::Instance()->quit();
                 break;
+            case SDL_MOUSEMOTION:
+                onMouseMove(event);
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                onMouseButtonDown(event);
+                break;
+            case SDL_MOUSEBUTTONUP:
+                onMouseButtonUp(event);
+                break;
             case SDL_KEYDOWN:
                 KeyEvents();
-
                 break;
             default:
                  break;
@@ -36,7 +52,7 @@ void InputHandler::update()
         }
 }
 
-//Tuşa basılıp basılmadığının kontrolü yapılır
+//Tusa basılıp basılmadıgının kontrolu yapilir.
 bool InputHandler::isKeyDown(SDL_Scancode key)
 {
     if(m_keystates != 0)
@@ -52,7 +68,7 @@ bool InputHandler::isKeyDown(SDL_Scancode key)
     }
     return false;
 }
-//Klavye yoluyla gerçekleşecek işlemleri yönetir
+//Klavye yoluyla gerceklesecek islemleri yonetir
 void InputHandler::KeyEvents()
 {
     m_keystates = SDL_GetKeyboardState(nullptr);
@@ -60,5 +76,50 @@ void InputHandler::KeyEvents()
     {
         TheSim::Instance()->quit();
     }
+
+}
+//Mouse'un x ve y koordinatı setlenir.
+void InputHandler::onMouseMove(SDL_Event& event)
+{
+    m_mousePosition.setX(event.motion.x);
+    m_mousePosition.setY(event.motion.y);
+}
+//Mouse'ta input verilip verilmediginin kontrolu yapilir.
+void InputHandler::onMouseButtonUp(SDL_Event& event) {
+	switch (event.button.button)
+	{
+        case SDL_BUTTON_LEFT:
+            m_mouseButtonStates[LEFT] = false;
+            break;
+        case SDL_BUTTON_MIDDLE:
+            m_mouseButtonStates[MIDDLE] = false;
+            break;
+        case SDL_BUTTON_RIGHT:
+            m_mouseButtonStates[RIGHT] = false;
+            break;
+        default:
+            break;
+	}
+}
+//Mouse'ta input verilip verilmediginin kontrolu yapilir.
+void InputHandler::onMouseButtonDown(SDL_Event& event) {
+	switch (event.button.button)
+	{
+        case SDL_BUTTON_LEFT:
+            m_mouseButtonStates[LEFT] = true;
+            break;
+        case SDL_BUTTON_MIDDLE:
+            m_mouseButtonStates[MIDDLE] = true;
+            break;
+        case SDL_BUTTON_RIGHT:
+            m_mouseButtonStates[RIGHT] = true;
+            break;
+        default:
+            break;
+	}
+}
+
+void InputHandler::clean()
+{
 
 }
