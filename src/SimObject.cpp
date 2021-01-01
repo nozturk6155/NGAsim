c6639ea8408729b032ccd567f7b4e0864ff093e2
Göktuğ Ersöz <qoktuqersoz.58@gmail.com>
2021-01-01 15:17:32 +0000

    SimObject.cpp dosyasında constuctor ve draw fonksiyonu parametreleri değiştirildi.

--- src/SimObject.cpp
+++ src/SimObject.cpp
@@ -2,20 +2,19 @@
 #include "Vector2D.h"
 #include "TextureManager.h"
 
-SimObject::SimObject(int x, int y, int w, int h, std::string textureID): m_position(0.0,0.0)
+SimObject::SimObject(int x, int y, int w, int h, std::string textureID): m_position(x,y)
 {
-    m_x = x;
-    m_y = y;
     m_width = w;
     m_height = h;
     m_textureID = textureID;
     m_currentFrame=0;
-
 }
-void SimObject::draw(SDL_Renderer* pRenderer)
+void SimObject::draw()
 {
-    TextureManager::Instance()->drawFrame(m_textureID, m_x, m_y,
-    m_width, m_height,m_currentFrame, pRenderer);
+
+    TextureManager::Instance()->drawFrame(m_textureID,(int)m_position.getX(),
+    (int)m_position.getY(),m_width, m_height,m_currentFrame,
+    TheSim::Instance()->getRenderer());
 
 }
 void SimObject::update()
