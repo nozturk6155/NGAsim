5b27b2eaaca83bf6c6d8097573e3779c08c10455
nozturk <nozturk6155@gmail.com>
2021-01-15 15:10:26 +0300

    Object.h: time.h include edildi. start ve diff degiskenleri zaman hesaplamasi icin eklendi started degiskeni bitis kontrolu icin eklendi Object.cpp: bitis kontrolu, zaman hesaplamasi ve ciktilar eklendi. hiz ve ivme degerleri ayarlandi. MainState.h: typeInbar1,typeInbar2,typeInbar3 fonksiyonlari eklendi input bar resmi yuklendi input bar nesneleri olusturuldu input bar nesneleri stacke yuklendi konsoldan input alan ve kontrol eden fonksiyonlar: typeInbar1,typeInbar2,typeInbar3 fonksiyonlari implemente edildi. main.cpp: 60 fps ayarlari eklendi.

--- include/Object.h
+++ include/Object.h
@@ -2,20 +2,25 @@
 #define OBJECT_H
 #include "SimObject.h"
 #include "SDL.h"
-
+#include "time.h"
 class Object : public SimObject
 {
     public:
         Object();
         Object(int x, int y, int w, int h, std::string id, float vx, float vy);
         void draw();
-        void update();
+        void update(); // par eklendi
         void clean();
         virtual ~Object();
+        int start;
+        double diff;
     private:
+        double m_x; // eklendi
+        double m_y; // eklendi
         Vector2D m_velocity;
         Vector2D m_acceleration;
         bool finished=false;
+        bool started=false;
 };
 
 #endif // OBJECT_H
--- src/MainState.cpp
+++ src/MainState.cpp
@@ -3,6 +3,8 @@
 #include "TextureManager.h"
 #include "Button.h"
 #include "RunState.h"
+#include "InputHolder.h"
+#include "InputBar.h"
 
 const std::string MainState::s_mainID = "MAIN";
 
@@ -46,31 +48,93 @@ bool MainState::onEnter()
     {
         return false;
     }
+    if(!TextureManager::Instance()->load("assets/inbar.png",TheSim::Instance()->getRenderer(), "inbar"))
+    {
+        return false;
+    }
     /* Create objects */
     SimObject* bilgi = new SimObject(0, 0, 150, 450, "bk");
     SimObject* inout = new SimObject(0, 450, 800, 150, "io");
     SimObject* hareket = new SimObject(150, 0, 650, 450, "he");
     SimObject* basla = new Button(185, 483, 50, 17, "basla", startToRun);
+    SimObject* inbar1 = new InputBar(110, 483, 50, 17, "inbar", typeInbar1);
+    SimObject* inbar2 = new InputBar(110, 516, 50, 17, "inbar", typeInbar2);
+    SimObject* inbar3 = new InputBar(110, 550, 50, 17, "inbar", typeInbar3);
 
     /* Push objects into to vector */
     m_simObject.push_back(bilgi);
     m_simObject.push_back(inout);
     m_simObject.push_back(hareket);
     m_simObject.push_back(basla);
+    m_simObject.push_back(inbar1);
+    m_simObject.push_back(inbar2);
+    m_simObject.push_back(inbar3);
 
     std::cout << "entering MainState\n";
     return true;
 
 }
 
+void MainState::typeInbar1()
+{
+    int yi;
+    std::cout << "Yi(ilk konum-y) giriniz: \n";
+    while(1)
+    {
+        std::cin >> yi;
+        if(yi > 0 && yi <= 390)
+        {
+            TheInputHolder::Instance()->setYi(yi);
+            break;
+        }
+        std::cout << "0 - 390 arasinda bir deger giriniz\n";
+    }
+}
+
+void MainState::typeInbar2()
+{
+    int vx;
+    std::cout << "Vx(x eksenindeki ilk hiz) giriniz: \n";
+    while(1)
+    {
+        std::cin >> vx;
+        if(vx >= 0 && vx <= 100)
+        {
+            TheInputHolder::Instance()->setVx(vx);
+            break;
+        }
+        std::cout << "0-100 arasinda bir deger giriniz\n";
+    }
+}
+
+void MainState::typeInbar3()
+{
+    int vy;
+    int dir;
+    std::cout << "Vy(y eksenindeki ilk hiz) giriniz: \n";
+    while(1)
+    {
+        std::cin >> vy;
+        if(vy >= 0 && vy <= 100)
+        {
+            TheInputHolder::Instance()->setVy(vy);
+        }
+        std::cout << "y eksenindeki hizin yonunu giriniz (1 yukari, 0 asagi): \n";
+        std::cin >> dir;
+        if(dir == 1 || dir == 0)
+        {
+            InputHolder::Instance()->setYon(dir);
+            break;
+        }
+        else
+        {
+            std::cout << "1 veya 0 giriniz\n";
+        }
+    }
+}
+
 void MainState::startToRun()
 {
-/*
-    InputHolder calistirilacak. TtfManager'dan stringleri alip
-    kontrol edecek düzenleyecek true or false dondurecek ve
-    tutacak.
-    Sonuca gore RunState'e gecilecek.
-*/
     if(TheSim::Instance()->getStateMachine()->getStateVector().back()->getStateID() != "RUN")
     {
         TheSim::Instance()->getStateMachine()->pushState(new RunState);
